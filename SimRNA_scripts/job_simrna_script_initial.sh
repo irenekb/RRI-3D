ae24492d042fa61a08831fcda07ced77593d7bf7
Irene Katharina Beckmann <irene@coridan.tbi.univie.ac.at>
2020-06-08 09:52:28 +0200

    SimRNA script update: error handling, cluster vers. local, random seed

--- SimRNA_scripts/job_simrna_script_initial.sh
+++ SimRNA_scripts/job_simrna_script_initial.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+## script started from job_simrna_start_initial.sh for a cluster-calculation
 #SBATCH --job-name=SimRNAinitial
 #SBATCH --time=144:0:0
 #SBATCH --mem-per-cpu=32G
@@ -14,9 +16,8 @@ SSFILE="$2"
 CSV3="$3"
 CSV4="$4"
 OUTPUT_DIRECTORY="$5"
-NAME="$6"
+SEEDTYPE="$7"
 
-NAME+="_"${SLURM_ARRAY_TASK_ID}
 
 #Change to local directory
 cd "$TMPDIR"
@@ -25,7 +26,17 @@ cd "$TMPDIR"
 cp -r "$CSV3" .
 
 #Run SimRNA
-echo SimRNA -s "$SEQFILE" -S "$SSFILE" -c "$CSV4" -R ${SLURM_ARRAY_TASK_ID} -o "$NAME" >& "$NAME".log 
+if [ "$SEEDTYPE" = "random" ]; then
+        for step in {1..10}; do
+		random=$(od -N 4 -t uL -An < /dev/urandom | tr -d " ")
+                # Reads 4 bytes from the random device and formats them as unsigned integer between 0 and 2^32-1
+		NAME="$6_${step}_${random}"
+		SimRNA -s "$SEQFILE" -S "$SSFILE" -c "$CSV4" -R "$random" -o "$NAME" >& "$NAME".log 
+	done
+else
+	NAME="$6_${SLURM_ARRAY_TASK_ID}_${SLURM_ARRAY_TASK_ID}"
+	SimRNA -s "$SEQFILE" -S "$SSFILE" -c "$CSV4" -R ${SLURM_ARRAY_TASK_ID} -o "$NAME" >& "$NAME".log
+fi
 
 #Copy files to output directory
 rm -r data
