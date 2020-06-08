8cef2a8a88b7eafd7679b507c3f42e8b5392e135
Irene Katharina Beckmann <irene@coridan.tbi.univie.ac.at>
2020-06-08 09:52:02 +0200

    SimRNA start update: error handling, cluster vers. local, random seed

--- SimRNA_scripts/job_simrna_start_initial.sh
+++ SimRNA_scripts/job_simrna_start_initial.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-##SimRNA_scripts/job_simrna_start_initial.sh SimRNA_interaction/1zci/ 1zci 00 ~/Programs/SimRNA_64bitIntel_Linux/ initial 
+## start a 16,000,000 million step run (stepsize 16,000) with a secondary structure and the sequence
+## SimRNA_scripts/job_simrna_start_initial.sh SimRNA_interaction/1zci/ 1zci 00 ~/Programs/SimRNA_64bitIntel_Linux/ initial local random
 
 INOUTDIR=$(realpath "$1")
 INITNAME="$2"
@@ -17,51 +18,69 @@ SIMRNADATA=${SIMRNA}/data/
 START="SimRNA_scripts/job_simrna_script_"${CONFIG}".sh"
 
 #Check if directories/files exist and all filenames/directories are correct
-echo 'Outputname' "$NAME"
+errors=()
 
 if [ ! -f "$START" ]; then
-    echo 'Sequence file does not exist!' "$START"
-    exit
-    else
-    echo "$START"
+	errors+=('Sequence file does not exist!' "$START")
+else
+       	echo "$START"
 fi
 
 if [ ! -d "$INOUTDIR" ]; then
-    echo 'In-/Output directory does not exist!' "$INOUTDIR"
-    exit
-    else
-    echo "$INOUTDIR"
+	errors+=('In-/Output directory does not exist!' "$INOUTDIR")
+else
+       	echo "$INOUTDIR"
 fi
 
 if [ ! -d "$SIMRNADATA" ]; then
-    echo 'SimRNA data directory does not exist!' "$SIMRNADATA" 
-    exit
-    else
-    echo "$SIMRNADATA"
+	errors+=('SimRNA data directory does not exist!' "$SIMRNADATA") 
+else
+	echo "$SIMRNADATA"
 fi
 
 if [ ! -f "$SEQFILE" ]; then
-    echo 'Sequence file does not exist!' "$SEQFILE"
-    exit
-    else
-    echo "$SEQFILE"
+	errors+=('Sequence file does not exist!' "$SEQFILE")
+else
+	echo "$SEQFILE"
 fi
 
 if [ ! -f "$SSFILE" ]; then
-    echo 'Secondary structure file does not exist!' "$SSFILE"
-    exit
-    else
-    echo "$SSFILE"
+	errors+=('Secondary structure file does not exist!' "$SSFILE")
+else
+	echo "$SSFILE"
 fi
 
 if [ ! -f "$SIMRNACONFIG" ]; then
-    echo 'SimRNA config file does not exist!' "$SIMRNACONFIG"
-    exit
-    else
-    echo "$SIMRNACONFIG"
+	errors+=('SimRNA config file does not exist!' "$SIMRNACONFIG")
+else
+	echo "$SIMRNACONFIG"
 fi
 
-  sbatch --output "$INOUTDIR"/"$NAME"_%j.log "$START" "$SEQFILE" "$SSFILE" "$SIMRNADATA" "$SIMRNACONFIG" "$INOUTDIR" "$NAME" &
 
+if [ ${#errors[@]} -eq 0 ] ; then
+	echo 'Outputname' "$NAME"
+else
+	shuf -e "${errors[@]}"
+	exit
+fi
+
+if [ "$6" = "local" ]; then
+	cp -r "$SIMRNADATA" .
+	if [ "$7" = "random" ]; then
+		for step in {1..10}; do
+			random=$(od -N 4 -t uL -An < /dev/urandom | tr -d " ")
+			# Reads 4 bytes from the random device and formats them as unsigned integer between 0 and 2^32-1	
+			NEWNAME="${INITNAME}_${CONFIG}_${ROUND}_${step}_${random}"
+			"SimRNA -s "$SEQFILE" -S "$SSFILE" -c "$SIMRNACONFIG" -R "$random" -o "$NEWNAME" >& "$NAME".log" 
+		done
+	else
+		for step in {1..10}; do
+			NEWNAME="${INITNAME}_${CONFIG}_${ROUND}_${step}_${step}"
+			"SimRNA -s "$SEQFILE" -S "$SSFILE" -c "$SIMRNACONFIG" -R "$step" -o "$NEWNAME" >& "$NAME".log" 
+		done
+	fi
+	rm -r data
+elif [ "$6" = "cluster" ]; then
+	sbatch --output "$INOUTDIR"/"$NAME"_%j.log "$START" "$SEQFILE" "$SSFILE" "$SIMRNADATA" "$SIMRNACONFIG" "$INOUTDIR" "$NAME" "$7" &
+fi
 wait
-
