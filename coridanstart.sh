50a6a829203f7d19a52ba36708e4f5810858fd95
Irene Katharina Beckmann <irene@coridan.tbi.univie.ac.at>
2022-05-30 16:36:27 +0200

    coridanstart.sh: bugfix cluster-1

--- coridanstart.sh
+++ coridanstart.sh
@@ -7,14 +7,14 @@ CLUSTER=$(awk -F= '$1=="CLUSTER"{print $2;exit}' $FILE)
 NAME=$(awk -F= '$1=="NAME"{print $2;exit}' $FILE)
 START=$(awk -F= '$1=="START"{print $2;exit}' $FILE)
 
-CLUSTER="$(($CLUSTER-"1"))"
 
 if [ $CLUSTER = "0" ]; then
   ./ultimatescript.sh $FILE "${NAME}" "$START"
 
 else
+  CLUSTER="$(($CLUSTER-"1"))"
 
-  for CST in `seq 0 1 ${CLUSTER}`; do
+  for CST in `seq 0 1 ${CLUSTER}`; do    
     cp "$START/${NAME}_0.il" "$START/cluster${CST}/${NAME}c${CST}_0.il" #check in startscript!
     ./ultimatescript.sh $FILE "${NAME}c${CST}" "$START/cluster${CST}"
   done
